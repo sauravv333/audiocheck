b0a4b68b9c8047e096286584bb6c9a7a5d204638
sauravv333 <sauravv333@gmail.com>
2023-08-17 23:46:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+// import "./styles.css";
+import WaveForm from "./WaveForm";
 
-function App() {
+export default function App() {
+  const [audioUrl, setAudioUrl] = useState();
+  const [analyzerData, setAnalyzerData] = useState(null);
+  const audioElmRef = useRef(null);
+
+  // audioAnalyzer function analyzes the audio and sets the analyzerData state
+  const audioAnalyzer = () => {
+    // create a new AudioContext
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    // create an analyzer node with a buffer size of 2048
+    const analyzer = audioCtx.createAnalyser();
+    analyzer.fftSize = 2048;
+
+    const bufferLength = analyzer.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    const source = audioCtx.createMediaElementSource(audioElmRef.current);
+    source.connect(analyzer);
+    source.connect(audioCtx.destination);
+    source.onended = () => {
+      source.disconnect();
+    };
+
+    // set the analyzerData state with the analyzer, bufferLength, and dataArray
+    setAnalyzerData({ analyzer, bufferLength, dataArray });
+  };
+
+  // onFileChange function handles the file input and triggers the audio analysis
+  const onFileChange = (e) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    setAudioUrl(URL.createObjectURL(file));
+    audioAnalyzer();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Audio Visualizer</h1>
+      {analyzerData && <WaveForm analyzerData={analyzerData} />}
+      <div
+        style={{
+          height: 100,
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center"
+        }}
+      >
+        <input type="file" accept="audio/*" onChange={onFileChange} />
+        <audio src={audioUrl ?? ""} controls ref={audioElmRef} />
+      </div>
     </div>
   );
 }
-
-export default App;
